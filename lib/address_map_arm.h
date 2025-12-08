e543035d4830aab2bd3a027fa73426d7946433b4
Fgodinez5060 <97177413+Fgodinez5060@users.noreply.github.com>
2025-12-07 19:31:01 -0700

    Add HEX display base address definition

--- lib/address_map_arm.h
+++ lib/address_map_arm.h
@@ -9,7 +9,7 @@
 #include <stdint.h>
 
 /* HPS peripheral base - from Activity 7 */
-#define HW_REGS_BASE           0xFC000000  // ALT_STM_OFST
+#define HW_REGS_BASE           0xFC000000 
 #define HW_REGS_SPAN           0x04000000
 #define HW_REGS_MASK           (HW_REGS_SPAN - 1)
 
@@ -20,6 +20,7 @@
 #define LED_PIO_BASE           0x3000      // LED base address
 #define DIPSW_PIO_BASE         0x4000      // DIP switch base address  
 #define BUTTON_PIO_BASE        0x5000      // Push button base address
+#define HEX_PIO_BASE           0x40070     // HEX display base address
 
 /* Data widths */
 #define LED_PIO_DATA_WIDTH     10          // 10 LEDs
